ff6bd4bef10a11d62bc2321ad7d594191487ee66
SrNadien 🔥 <76491773+SrNadien@users.noreply.github.com>
2026-05-13 17:32:51 -0300

    Bump Version: 1.0.0

---
+++ src/main/java/nadiendev/ae2nocrater/AE2NoMeteorCrates.java
@@ -0,0 +1,13 @@
+package nadiendev.ae2nometeorcrates;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(AE2NoMeteorCrates.MODID)
+public class AE2NoMeteorCrates {
+
+    public static final String MODID = "ae2nometeorcrates";
+
+    public AE2NoMeteorCrates(IEventBus modEventBus) {
+    }
+}
---
+++ src/main/java/nadiendev/ae2nocrater/mixin/MeteoriteStructurePieceMixin.java
@@ -0,0 +1,25 @@
+package nadiendev.ae2nometeorcrates.mixin;
+
+import appeng.worldgen.meteorite.CraterType;
+import appeng.worldgen.meteorite.MeteoriteStructurePiece;
+import appeng.worldgen.meteorite.fallout.FalloutMode;
+import net.minecraft.core.BlockPos;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(value = MeteoriteStructurePiece.class, remap = false)
+public class MeteoriteStructurePieceMixin {
+
+    @ModifyArg(
+        method = "<init>(Lnet/minecraft/core/BlockPos;FLappeng/worldgen/meteorite/CraterType;Lappeng/worldgen/meteorite/fallout/FalloutMode;ZZ)V",
+        at = @At(
+            value = "INVOKE",
+            target = "Lappeng/worldgen/meteorite/PlacedMeteoriteSettings;<init>(Lnet/minecraft/core/BlockPos;FLappeng/worldgen/meteorite/CraterType;Lappeng/worldgen/meteorite/fallout/FalloutMode;ZZ)V"
+        ),
+        index = 2
+    )
+    private CraterType ae2nometeorcrates$forceCraterTypeNone(CraterType original) {
+        return CraterType.NONE;
+    }
+}
